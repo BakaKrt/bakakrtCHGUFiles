c3dc0d1ba8dc0f9625701fdb19f25c4ff1998425
BakaKrt <eardykov@gmail.com>
2022-12-15 22:49:23 +0300

    upd 7th

--- C++/test7b/test7b.cpp
+++ C++/test7b/test7b.cpp
@@ -1,49 +1,63 @@
-﻿//  TEST7B – ïðîãðàììà ñ îøèáêàìè
-#include <iostream>
+﻿#include <iostream>
+#include <iomanip>
 #include <cstring>
 using namespace std;
-
 int main()
 {
-	struct TPerson {
-		struct {
-			typedef char TStroka name; // ôàìèëèÿ
-			TStroka name1; // èìÿ
-		} name;
-		unsigned char Degree : 3;
-		char Ord[8];;
-	};
-	struct TBook {
-		TPerson Auther;
-		TStroka	Title;
-		struct Publ {
-			unsigned char City : 3;
-			int Year;
-		}; char Subj[16];
-	};
-	typedef char TStroka[16];
-	TBook books[100];
-	int i, j, N, f;
+	setlocale(0, "");
+	const int countMax = 29;
 
-	cin >> N;
-	if (f == 100) N = 100;
-	for (i = 0; i < N; i = 1 + i) {
-		cin >> books[i].Auther.name;
-		cin >> j;
-		books[i].Auther.Degree = j;
-		cin >> books[i].Auther.Ord;
-		cin >> books[i].Title;
-		cin >> books[i].Publ.Year;
-		cin >> j;
-		books[i].Publ.City = j;
-		j = 0;
-		do {
-			cin >> books[j].Subj;
-			j = j + 1;
-		} while (books[i].Subj != ' ');
+	struct STUD
+	{
+		char NAME[16];
+		int ATTESTATION[6] = { 1,1,1,1,1,1 };
+		float STEPENDIA = { 0 };
+	};
+	STUD GROUP[countMax];
+
+	int countStud, Num, OZENKA; // число и номер студента, оценка по аттестату
+	printf("Число студентов (до %d): ", countMax + 1);
+	cin >> countStud;
+
+	if (countStud > countMax) countStud = countMax;
+	if (countStud < 1) countStud = 1;
+
+	for (Num = 0; Num < countStud; Num++) {
+		cout << "\tСтудент номер:\n" << Num + 1 << ". "
+			<< setw(28) << left << "Имя?";
+		cin >> GROUP[Num].NAME;
+		cout << " Аттестат (алгебра, геометрия, русский язык, литература, информатика, география) [1-удовл, 2-хорошо, 3-отлично]: ";
+		for (int attest_len = 0; attest_len != 6; attest_len++) {
+			cin >> OZENKA;
+			if (OZENKA > 3) OZENKA = OZENKA % 3;
+			GROUP[Num].ATTESTATION[attest_len] = OZENKA;
+		}
+		for (int attest_len = 0; attest_len != 6; attest_len++) {
+			cout << attest_len + 1 << "-ый предмет:\t" << GROUP[Num].ATTESTATION[attest_len] << endl;
+		}
+		cout << "Размер степухи (float): ";
+		cin >> GROUP[Num].STEPENDIA;
 	}
-	for (j = 1; i < N; i++)
-		books[i].Subj = 'computer';
+	bool is_step = false;
+	for (Num = 0; Num < countStud; Num++) {
+		if (GROUP[Num].STEPENDIA != 0) {
+			if (is_step == false) {
+				cout << "У кого есть степуха:\n";
+				is_step = true;
+			}
+			cout << "Имя:";
+			cout << "\t" << GROUP[Num].NAME << endl;
+			cout << "Оценки по аттестату: ";
+			for (int i = 0; i < 6; i++) {
+				cout << "\t" << GROUP[Num].ATTESTATION[i];
+			}
+			cout << "\nСтепендия:";
+			cout << "\t" << GROUP[Num].STEPENDIA << endl;
+		}
+	}
+	if (is_step == false)
+		cout << "Видимо всех отчислили, ни у кого нет степухи :(";
 
+	cin.get();
 	return 0;
 }
